ad1811d2d9c22a062f88c3480b74b6a7c1195177
B1oodyAnge1 <romsm@bk.ru>
2024-08-03 17:23:24 +0300

    The task is completed

--- Comparing_numbers_using_the_branching_operator/Comparing_numbers_using_the_branching_operator.cpp
+++ Comparing_numbers_using_the_branching_operator/Comparing_numbers_using_the_branching_operator.cpp
@@ -1,7 +1,34 @@
 ﻿#include <iostream>
+#include <string>
+#include <regex>
+
+//Составить программу, которая требует ввести два числа.Если первое число больше второго, то программа печатает слово больше.
+//Если первое число меньше второго, то программа печатает слово меньше.
+//А если числа равны, программа напечатает сообщение Эти числа равны.
+
+int inputInt(std::string textReq = "Enter an integer number") { // textReq текстовый запрос у пользователя
+    std::string number;
+    std::regex reg("^[0-9]"); //регулярное выражение
+    while (true)
+    {
+        std::cout << textReq << std::endl;
+        std::cin >> number;
+        if (std::regex_search(number, reg)) {
+            return stoi(number); //преобразование string в int. С++ 11
+        }
+    }
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "ru");
+	int a = 0, b = 0;
+    a = inputInt("Введите 1е число:");
+    b = inputInt("Введите 2е число:");
+    if (a > b) { std::cout << "Больше\n";}
+    if (a < b) { std::cout << "Меньше\n"; }
+    if (a == b) { std::cout << "Равно\n"; }
+	return 0;
 }
 
